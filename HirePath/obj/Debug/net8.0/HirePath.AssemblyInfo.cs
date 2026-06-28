be5ab29062bb6c7ef8e5b44863177999ca1d34fe
chamodyha123 <kcpkehelgamuwa@students.nsbm.ac.lk>
2026-06-28 12:11:57 +0530

    ai layer is done

--- HirePath/obj/Debug/net8.0/HirePath.AssemblyInfo.cs
+++ HirePath/obj/Debug/net8.0/HirePath.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HirePath")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+654853870298d93e8023613c6778a94a8f1dc1aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4e098c2842d9321b3321a41696497b193f5bae7")]
 [assembly: System.Reflection.AssemblyProductAttribute("HirePath")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HirePath")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
