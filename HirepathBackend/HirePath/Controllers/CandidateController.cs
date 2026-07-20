9892bb82f191c99fd33e6bc53f0c4dea29893bbb
chamodyha123 <kcpkehelgamuwa@students.nsbm.ac.lk>
2026-07-20 21:34:45 +0530

    chvsavcvhc

--- HirepathBackend/HirePath/Controllers/CandidateController.cs
+++ HirepathBackend/HirePath/Controllers/CandidateController.cs
@@ -1,4 +1,4 @@
-﻿using HirePathAI.API.DTOs.Candidate;
+using HirePathAI.API.DTOs.Candidate;
 using HirePathAI.API.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.JsonPatch;
@@ -8,7 +8,8 @@ using System.Security.Claims;
 namespace HirePathAI.API.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/Candidate")]
+    [Route("api/candidates")]
     [Authorize]
     public class CandidateController : ControllerBase
     {
